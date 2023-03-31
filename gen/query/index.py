74d2313206312679a841e0a2df45b4370b791046
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-03-31 10:56:29 +0000

    App running, but needs to fix bug

--- gen/query/index.py
+++ gen/query/index.py
@@ -1,19 +1,22 @@
-from flask import Flask, request
-import openai
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+import requests
+import openai
 import os
-
-app = Flask(__name__)
 
 # Load environment variables from .env file
 load_dotenv()
 
-openai.api_key = os.getenv('API_KEY')
+app = Flask(__name__)
+
+api_key = os.getenv('API_KEY')
+openai.api_key = api_key
 
 def generate_response(prompt):
     # Set up the OpenAI API parameters
     model_engine = "davinci" # or another model from OpenAI's API
-    prompt_text = "Generate a SQL Query based on this " + prompt + " only return sql query, no explanations and conclusions, if " + prompt + " does not contain enough information to make up a SQL query, return with the following text Please provide enough information to makeup a query "
+    # prompt_text = "Generate a SQL Query based on this " + prompt + " only return sql query, no explanations and conclusions, if " + prompt + " does not contain enough information to make up a SQL query, return with the following text Please provide enough information to makeup a query "
+    prompt_text = prompt
     max_tokens = 50
     temperature = 0.5
     n = 1
@@ -38,9 +41,12 @@ def index():
 
 @app.route('/generate_response', methods=['POST'])
 def get_response():
-    data = request.get_json()
-    prompt = data['prompt']
+    message = request.form.get('message')
 
+    if message is None:
+        return 'Error: no message provided'
+
+    prompt = "Please generate a MySQL query from the following text without explanation: " + message
     # Generate the response using the OpenAI API
     response = generate_response(prompt)
 
