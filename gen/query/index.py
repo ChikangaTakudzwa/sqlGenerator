99324724a31788c9dc10eb2a377210e4fb01ad9d
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-03-31 16:29:53 +0000

    App working, response coming of SQL Generated

--- gen/query/index.py
+++ gen/query/index.py
@@ -14,8 +14,7 @@ openai.api_key = api_key
 
 def generate_response(prompt):
     # Set up the OpenAI API parameters
-    model_engine = "davinci" # or another model from OpenAI's API
-    # prompt_text = "Generate a SQL Query based on this " + prompt + " only return sql query, no explanations and conclusions, if " + prompt + " does not contain enough information to make up a SQL query, return with the following text Please provide enough information to makeup a query "
+    model_engine = "davinci"
     prompt_text = prompt
     max_tokens = 50
     temperature = 0.5
@@ -46,7 +45,7 @@ def get_response():
     if message is None:
         return 'Error: no message provided'
 
-    prompt = "Please generate a MySQL query from the following text without explanation: " + message
+    prompt = "Please generate a MySQL query from the following text, only query without explanation: " + message
     # Generate the response using the OpenAI API
     response = generate_response(prompt)
 
